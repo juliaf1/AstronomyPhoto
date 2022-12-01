be729e47aabad42e1f5cf21ff2e5230aa6c91be9
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-01 17:44:15 -0300

    feat: add today and search view controllers classes

---
+++ AstronomyPhoto/Controllers/ViewControllers/TodayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodayViewController.swift
+//  AstronomyPhoto
+//
+//  Created by Julia Frederico on 01/12/22.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
