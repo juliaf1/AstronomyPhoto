62007a0acfcb2c1ee31f91803a29b547228c64b5
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-01 17:25:51 -0300

    feat: add custom inter font

---
+++ AstronomyPhoto/Helpers+Extensions/Fonts.swift
@@ -0,0 +1,26 @@
+//
+//  Fonts.swift
+//  AstronomyPhoto
+//
+//  Created by Julia Frederico on 01/12/22.
+//
+
+import UIKit
+
+struct Fonts {
+    
+    static let InterLight = UIFont(name: "Inter-Light", size: UIFont.labelFontSize)
+    static let InterRegular = UIFont(name: "Inter-Regular", size: UIFont.labelFontSize)
+    static let InterMedium = UIFont(name: "Inter-Medium", size: UIFont.labelFontSize)
+    static let InterBold = UIFont(name: "Inter-Bold", size: UIFont.labelFontSize)
+    
+    
+    // BOILERPLATE CODE - updating label font & size // remove later!
+//    let label: UILabel = {
+//        let label = UILabel()
+//        label.font = Fonts.InterLight
+//        label.font.withSize(300)
+//        return label
+//    }()
+    
+}
