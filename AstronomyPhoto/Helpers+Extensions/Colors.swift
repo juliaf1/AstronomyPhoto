199d4ccc9bb3683d675e448ef398d6aa30cd1e5b
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-01 17:42:57 -0300

    feat: add colors struct

---
+++ AstronomyPhoto/Helpers+Extensions/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  AstronomyPhoto
+//
+//  Created by Julia Frederico on 01/12/22.
+//
+
+import UIKit
+
+struct Colors {
+    
+    static let white = UIColor(named: "AppWhite")
+    static let black = UIColor(named: "AppBlack")
+    static let zinc1 = UIColor(named: "AppZinc1")
+    static let zinc2 = UIColor(named: "AppZinc2")
+    static let zinc3 = UIColor(named: "AppZinc3")
+    static let brandPrimary = UIColor(named: "AppBrandPrimary")
+    static let brandSecondary = UIColor(named: "AppBrandSecondary")
+    
+}
--- AstronomyPhoto/Views/CustomViews/CustomTabBar.swift
+++ AstronomyPhoto/Views/CustomViews/CustomTabBar.swift
@@ -22,7 +22,7 @@ class CustomTabBar: UITabBar {
         
         self.layer.shadowOffset = CGSize(width: 0, height: 0)
         self.layer.shadowRadius = 5
-        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowColor = Colors.black?.cgColor
         self.layer.shadowOpacity = 0.3
     }
 
