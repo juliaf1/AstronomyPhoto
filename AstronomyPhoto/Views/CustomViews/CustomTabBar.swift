f92ba32de33a2a5b7260ae3780c6ff02463fbd5f
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-01 17:39:40 -0300

    feat: add shadow to tab bar

--- AstronomyPhoto/Views/CustomViews/CustomTabBar.swift
+++ AstronomyPhoto/Views/CustomViews/CustomTabBar.swift
@@ -10,15 +10,20 @@ import UIKit
 class CustomTabBar: UITabBar {
 
     override func sizeThatFits(_ size: CGSize) -> CGSize {
-         var size = super.sizeThatFits(size)
-         size.height = 100
-         return size
+        var size = super.sizeThatFits(size)
+        size.height = 100
+        return size
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
         self.layer.cornerRadius = 12
+        
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.3
     }
 
 }
