75494314eeae8b1fe0dae54c19e1e1ae76268143
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-01 23:36:35 -0300

    feat: add animations to search vc

--- AstronomyPhoto/Controllers/ViewControllers/SearchViewController.swift
+++ AstronomyPhoto/Controllers/ViewControllers/SearchViewController.swift
@@ -15,6 +15,7 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var startDateTextField: UITextField!
     @IBOutlet weak var endDateTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var titleLabel: UILabel!
 
     // MARK: - Lifecycle
     
@@ -22,12 +23,25 @@ class SearchViewController: UIViewController {
         super.loadView()
         
         layoutViews()
+        hideViews()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureViews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        displayViews()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        hideViews()
     }
     
     // MARK: - Actions
@@ -46,6 +60,26 @@ class SearchViewController: UIViewController {
     func configureViews() {
         startDateTextField.delegate = self
         endDateTextField.delegate = self
+    }
+    
+    func hideViews() {
+        titleLabel.layer.opacity = 0
+        searchCard.layer.opacity = 0
+        
+        titleLabel.isHidden = true
+        searchCard.isHidden = true
+    }
+    
+    func displayViews() {
+        UIView.animate(withDuration: 0.4, delay: 0, options: [.curveEaseOut]) {
+            self.titleLabel.isHidden = false
+            self.searchCard.isHidden = false
+        }
+        
+        UIView.animate(withDuration: 0.8, delay: 0, options: [.curveEaseOut]) {
+            self.titleLabel.layer.opacity = 1
+            self.searchCard.layer.opacity = 1
+        }
     }
 
 }
