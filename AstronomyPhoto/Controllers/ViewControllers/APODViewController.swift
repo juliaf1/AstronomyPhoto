9635f2d268c071d26d2bc1fb21a97448ff8c7159
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-02 14:05:36 -0300

    feat: implement close button tap on apod detail view

--- AstronomyPhoto/Controllers/ViewControllers/APODViewController.swift
+++ AstronomyPhoto/Controllers/ViewControllers/APODViewController.swift
@@ -11,18 +11,27 @@ class APODViewController: UIViewController {
     
     // MARK: - Properties
     
+    var apod: APOD? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var closeLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var favoriteButton: UIButton!
     
-    // MARK: - Outlets
-    
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureViews()
     }
     
     // MARK: - Actions
@@ -33,5 +42,26 @@ class APODViewController: UIViewController {
     }
 
     // MARK: - Helpers
+    
+    func updateViews() {
+        guard let apod = apod else {
+            return
+        }
+
+        dateLabel.text = apod.date.toString()
+        photoImageView.image = apod.photo
+        titleLabel.text = apod.title
+        descriptionLabel.text = apod.description
+    }
+    
+    func configureViews() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissView))
+        closeLabel.isUserInteractionEnabled = true
+        closeLabel.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissView() {
+        self.dismiss(animated: true)
+    }
 
 }
