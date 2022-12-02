0af0c04e53a59536f4d9251448455a53dda0b63f
Julia Frederico <magalhaesjulia@id.uff.br>
2022-12-02 20:36:30 -0300

    feat: add search vc and scene

---
+++ AstronomyPhoto/Controllers/ViewControllers/FavoritesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesViewController.swift
+//  AstronomyPhoto
+//
+//  Created by Julia Frederico on 02/12/22.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Helpers
+}
